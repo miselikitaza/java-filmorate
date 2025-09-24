b58cc9077bfa832ce6e48dc93b6aa69b466afc98
User <mitinareg.@yandex.ru>
2025-09-24 19:27:43 +0300

    Создать классы - модели данных приложения. Аннотировать их так, чтобы автоматически создавались геттеры, сеттеры и т.д.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,14 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 import java.time.LocalDate;
 
-/**
- * Film.
- */
 @Data
-public class Film {
+public class User {
     private Long id;
+    private String login;
     private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private Long duration;
+    private String email;
+    private LocalDate birthday;
+
 }
