1bca6e89ca5b9cb4695c60ea4cb76319d676d140
User <mitinareg.@yandex.ru>
2025-10-01 23:18:37 +0300

    Добавить аннотации для проверки полей

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,15 +2,28 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import java.time.LocalDate;
+import jakarta.validation.constraints.*;
 
 /**
  * Film.
  */
 @Data
 public class Film {
+
+    private static final int MAX = 200;
+
     private Long id;
+
+    @NotBlank(message = "Название не может быть пустым")
     private String name;
+
+    @Size(max = MAX, message = "Максимальная длина описания — 200 символов")
     private String description;
+
+    @NotNull
     private LocalDate releaseDate;
+
+    @NotNull
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private Long duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 public class User {
+
     private Long id;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не должен содержать пробелы")
     private String login;
+
     private String name;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Почта должна содержать символ @")
     private String email;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 
 }
