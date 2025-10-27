c02c43a937927d460bfda5dc1fe70e512584d247
Regina <mitina.379@mail.ru>
2025-10-27 12:22:56 +0300

    Создать интерфейс для базовых методов с бъектами класса Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface FilmStorage {
+
+    public Film createFilm(Film film);
+
+    public Film updateFilm(Film film);
+
+    public Film getFilmById(Long id);
+
+    public List<Film> getAllFilms();
+
+    public void deleteFilmById(Long id);
+
+    public void deleteAllFilms();
+}
