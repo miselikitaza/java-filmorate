3e78b1c734432018affa6ecdb02273f52c192c84
Regina <mitina.379@mail.ru>
2025-10-27 12:21:24 +0300

    Добавить множество лайков. Добавить аннотацию для ненулевого id фильма при обновлении

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,16 +2,17 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.validation.constraints.*;
 
-/**
- * Film.
- */
 @Data
 public class Film {
 
     private static final int MAX = 200;
 
+    @NotNull(groups = Update.class, message = "ID не может быть пустым")
     private Long id;
 
     @NotBlank(message = "Название не может быть пустым", groups = Create.class)
@@ -25,6 +26,8 @@ public class Film {
     @Positive(message = "Продолжительность фильма должна быть положительным числом",
             groups = {Create.class, Update.class})
     private Long duration;
+
+    private Set<Long> likes = new HashSet<>();
 
     public interface Create {}
 
