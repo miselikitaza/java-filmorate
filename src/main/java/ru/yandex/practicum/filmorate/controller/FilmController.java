88c9ee25639be4c76662dca302672bbad450b7bb
Regina <mitina.379@mail.ru>
2025-10-27 12:30:27 +0300

    Избавить класс от бизнес-логики. Оставить лишь обработку путей запросов и вызов необходимой валидации

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,88 +1,72 @@
 package ru.yandex.practicum.filmorate.controller;
 
-
-import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
 
 import java.time.LocalDate;
 import java.util.*;
 
-@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    private static final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
+    private final FilmService filmService;
 
-    private final Map<Long, Film> films = new HashMap<>();
+    @Autowired
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @PostMapping
-    public Film createFilm(@Validated(Film.Create.class) @RequestBody Film film) {
-        log.info("Получен запрос на создание фильма {}", film.getName());
-        validateReleaseDate(film);
-        film.setId(getNextId());
-        films.put(film.getId(), film);
-        log.info("Фильм {} успешно создан", film.getName());
-        return film;
+    public Film create(@Validated({Film.Create.class}) @RequestBody Film film) {
+        return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Validated(Film.Update.class) @RequestBody Film newFilm) {
-        if (newFilm.getId() == null) {
-            log.error("Попытка обновления фильма без указания ID");
-            throw new ConditionsNotMetException("ID не может быть пустым");
-        }
-        Film oldFilm = films.get(newFilm.getId());
-        if (oldFilm == null) {
-            log.warn("Фильм с ID: {} не найден", newFilm.getId());
-            throw new NotFoundException("Фильм с ID: " + newFilm.getId() + " не найден");
-        }
-        validateReleaseDate(newFilm);
-        if (newFilm.getName() != null) {
-            oldFilm.setName(newFilm.getName());
-        }
-        if (newFilm.getDescription() != null) {
-            oldFilm.setDescription(newFilm.getDescription());
-        }
-        if (newFilm.getReleaseDate() != null) {
-            oldFilm.setReleaseDate(newFilm.getReleaseDate());
-        }
-        if (newFilm.getDuration() != null) {
-            oldFilm.setDuration(newFilm.getDuration());
-        }
-        log.info("Фильм с ID: {} успешно обновлен", oldFilm.getId());
-        return oldFilm;
+        return filmService.updateFilm(newFilm);
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable Long id) {
+        return filmService.getFilmById(id);
     }
 
     @GetMapping
-    public Collection<Film> getAllFilms() {
-        log.info("Получен запрос на получение всех фильмов. Количество: {}", films.size());
-        return films.values();
+    public List<Film> getAllFilms() {
+        return filmService.getAllFilms();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFilmById(@PathVariable Long id) {
+        filmService.deleteFilmById(id);
     }
 
     @DeleteMapping
     public void deleteAllFilms() {
-        films.clear();
+        filmService.deleteAllFilms();
     }
 
-    private void validateReleaseDate(Film film) {
-        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(MIN_DATE)) {
-            log.warn("Некорректная дата релиза");
-            throw new ConditionsNotMetException("Дата релиза должна быть не раньше 28 декабря 1895 года");
-        }
+    @PutMapping("/{id}/like/{userId}")
+    public void like(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.like(id, userId);
     }
 
-    private long getNextId() {
-        long currentMaxId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.deleteLike(id, userId);
     }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10") @Positive int count) {
+        return filmService.getPopularFilms(count);
+    }
+
 }
