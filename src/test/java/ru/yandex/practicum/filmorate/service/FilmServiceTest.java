5b802bfc25d735a0727ad763de0849cc15a7cf2b
Regina <mitina.379@mail.ru>
2025-10-29 16:47:17 +0300

    Удалить неиспользуемые импорты

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
-import ru.yandex.practicum.filmorate.service.user.UserService;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
